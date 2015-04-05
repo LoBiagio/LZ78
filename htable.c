31aff66554cafe7e27d2d066b820b3780f90ecd7
Lorenzo Biagini <lorebiagio@gmail.com>
2015-04-05 19:20:17 +0200

    Update htable.c
    I inserted "typedef struct htable TABLE" in htable.h (not in htable.c) because a declaration like "TABLE table" in a file with just the header "htable.h" included would fail (the compiler wouldn't know how much it should allocate for type TABLE).

--- htable.c
+++ htable.c
@@ -2,19 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-typedef struct
+struct hentry
 {
     uint64_t *value;
     int len;
-} ENTRY;
+};
 
-typedef struct htable
-{
-    ENTRY *entries;
-    int nmemb;
-    int dim;
-} TABLE;
 
 int htable_init(TABLE *table, int size) {
     table->nmemb = 0;
@@ -43,69 +38,86 @@ int htable_nmemb(TABLE *table) {
     return table->nmemb;
 }
 
-uint64_t get_hash(uint64_t *str, int len) {
+uint64_t get_hash(uint64_t *value, int len) {
     uint64_t hash = 5381lu, c;
     int i;
 
     for (i = 0; i < len; i++) {
-        c = str[i];
+        c = value[i];
         hash = ((hash << 5) + hash) + c;
     }
 
     return hash;
 }
 
+int htable_search(TABLE *table, uint64_t *value, int len, uint64_t *index) {
+    int uint64_len = len % 64 != 0 ? len / 64 + 1 : len / 64;
+    int search = 0;
+    *index = get_hash(value, uint64_len);
+    *index %= table->dim;
+    while (search != table->dim) {
+        // Element does not exist, index is on the first free array cell found
+        if (table->entries[*index].len == 0) {
+            return 0;
+        }
+        // Element found
+        if (len == table->entries[*index].len && memcmp(value, table->entries[*index].value, uint64_len)) {
+                return 1;
+        }
+        ///////////////////////
+
+        printf("COLLISION\n");
+
+        ///////////////////////
+        *index = (*index + 1) % table->dim;
+        search++;
+    }
+    // Element not found
+    return 0;
+}
 /* Insert in table an entry value of len bits */
 int htable_insert(TABLE *table, uint64_t *value, int len) {
-    uint64_t hash;
+    uint64_t index;
 
     // Table is full
     if (table->nmemb == table->dim) {
         return -1;
     }
 
-    hash = get_hash(value, len);
-    hash %= table->dim;
-    while (table->entries[hash].len) {
-        hash = (hash + 1) % table->dim;
+    // Element already exists
+    if (!htable_search(table, value, len, &index)) {
+        return -1;
     }
 
-    table->entries[hash].value = (uint64_t *)calloc((len % 64 != 0 ? len / 64 + 1 : len / 64), sizeof(uint64_t));
+    table->entries[index].value = (uint64_t *)calloc((len % 64 != 0 ? len / 64 + 1 : len / 64), sizeof(uint64_t));
     // Allocation fails
-    if (table->entries[hash].value == NULL) {
+    if (table->entries[index].value == NULL) {
         return -1;
     }
-    if(memcpy(table->entries[hash].value, value, len) == NULL) {
-        free(table->entries[hash].value);
+    if(memcpy(table->entries[index].value, value, len) == NULL) {
+        free(table->entries[index].value);
         return -1;
     }
-    table->entries[hash].len = len;
+    table->entries[index].len = len;
     table->nmemb++;
     return 1;
 }
 
 /* Remove from table an entry value of len bits */
 int htable_remove(TABLE *table, uint64_t *value, int len) {
-    uint64_t hash;
-    int search = 0;
+    uint64_t index;
 
     // Empty table
     if (table->nmemb == 0) {
         return -1;
     }
 
-    hash = get_hash(value, len);
-    hash %= table->dim;
-    while (!memcmp(table->entries[hash].value, value, (len % 64 != 0 ? len / 64 + 1 : len / 64)) && search != table->dim) {
-        hash = (hash + 1) % table->dim;
-        search++;
-    }
     // Element not found
-    if (search == table->dim) {
+    if (!htable_search(table, value, len, &index)) {
         return -1;
     }
-    free(table->entries[hash].value);
-    table->entries[hash].len = 0;
+    free(table->entries[index].value);
+    table->entries[index].len = 0;
     table->nmemb--;
     return 0;
 }
