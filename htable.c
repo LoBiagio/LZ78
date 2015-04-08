1d61f4bc21f20fa07f1feae19d9160c76ebdda5a
Lorenzo Biagini <lorebiagio@gmail.com>
2015-04-08 19:30:25 +0200

    Update htable.c

--- htable.c
+++ htable.c
@@ -4,21 +4,35 @@
 #include <string.h>
 #include <stdio.h>
 
-struct hentry
+typedef struct hentry
 {
-    uint64_t *value;
+    char *value;
     int len;
+} ENTRY;
+
+struct htable
+{
+    ENTRY *entries;
+    int nmemb;
+    int dim;
 };
 
-
-int htable_init(TABLE *table, int size) {
-    table->nmemb = 0;
-    table->entries = (ENTRY *)calloc(size + (size / 2), sizeof(ENTRY));
-    if (table->entries == NULL) {
-        return -1;
+TABLE *htable_new(int size) {
+    TABLE *table = (TABLE *)calloc(1, sizeof(TABLE));
+    if (table == NULL) {
+        printf("Error: table allocation failed.\n");
+        return NULL;
     }
-    table->dim = size + (size / 2);
-    return 1;
+    table->nmemb = 0;
+    table->entries = (ENTRY *)calloc(size, sizeof(ENTRY));
+    if (table->entries == NULL) {
+        printf("Error: dictionary allocation failed.\n");
+        table->dim = 0;
+        free(table);
+        return NULL;
+    }
+    table->dim = size;
+    return table;
 }
 
 void htable_clear(TABLE *table){
@@ -38,63 +52,78 @@ int htable_nmemb(TABLE *table) {
     return table->nmemb;
 }
 
-uint64_t get_hash(uint64_t *value, int len) {
-    uint64_t hash = 5381lu, c;
+unsigned int get_hash(char *value, int len) {
+    unsigned int hash = 5381u;
     int i;
 
     for (i = 0; i < len; i++) {
-        c = value[i];
-        hash = ((hash << 5) + hash) + c;
+        hash = ((hash << 5) + hash) + value[i];
     }
 
     return hash;
 }
 
-int htable_search(TABLE *table, uint64_t *value, int len, uint64_t *index) {
-    int uint64_len = len % 64 != 0 ? len / 64 + 1 : len / 64;
-    int search = 0;
-    *index = get_hash(value, uint64_len);
-    *index %= table->dim;
+/* index is the position where the value has been found or can be stored */
+int htable_search(TABLE *table, uint64_t *value, int len, unsigned int *index) {
+    int len_bytes = len % 8 != 0 ? len / 8 + 1 : len / 8;
+    int search = 0, found = 0;
+    unsigned int hash;
+    hash = get_hash((char *)value, len_bytes);
+    hash %= table->dim;
     while (search != table->dim) {
-        // Element does not exist, index is on the first free array cell found
-        if (table->entries[*index].len == 0) {
-            return 0;
+        // Element does not exist, hash is on the first free array cell found
+        if (table->entries[hash].len == 0) {
+            break;
         }
         // Element found
-        if (len == table->entries[*index].len && memcmp(value, table->entries[*index].value, uint64_len)) {
-                return 1;
+        if (len == table->entries[hash].len && !memcmp(value, table->entries[hash].value, len_bytes)) {
+            found = 1;
+            break;
         }
-        ///////////////////////
-
-        printf("COLLISION\n");
-
-        ///////////////////////
-        *index = (*index + 1) % table->dim;
+        
+        //* Delete this block *//
+        if (DETECT_COLLISION) {
+            DETECT_COLLISION = 0;
+            COLLISIONS++;
+            printf("Collision\n");
+        }
+        //*********************//
+        
+        hash = (hash + 1) % table->dim;
         search++;
     }
-    // Element not found
-    return 0;
+    if (index != NULL) {
+        *index = hash;
+    }
+    return found;
 }
-/* Insert in table an entry value of len bits */
+
+/* Insert in table a value of len bits */
 int htable_insert(TABLE *table, uint64_t *value, int len) {
-    uint64_t index;
+    unsigned int index;
+    int len_bytes;
 
     // Table is full
     if (table->nmemb == table->dim) {
+        printf("Error: the dictionary is full.\n");
         return -1;
     }
 
     // Element already exists
-    if (!htable_search(table, value, len, &index)) {
+    if (htable_search(table, value, len, &index)) {
+        printf("Error: the value is already in the dictionary.\n");
         return -1;
     }
 
-    table->entries[index].value = (uint64_t *)calloc((len % 64 != 0 ? len / 64 + 1 : len / 64), sizeof(uint64_t));
+    len_bytes = len % 8 != 0 ? len / 8 + 1 : len / 8;
+    table->entries[index].value = (char *)calloc(len_bytes, sizeof(char));
     // Allocation fails
     if (table->entries[index].value == NULL) {
+        printf("Error: allocation for new entry in dictionary failed.\n");
         return -1;
     }
-    if(memcpy(table->entries[index].value, value, len) == NULL) {
+    if(memcpy(table->entries[index].value, value, len_bytes) == NULL) {
+        printf("Error: copying new entry in dictionary failed.\n");
         free(table->entries[index].value);
         return -1;
     }
@@ -105,15 +134,17 @@ int htable_insert(TABLE *table, uint64_t *value, int len) {
 
 /* Remove from table an entry value of len bits */
 int htable_remove(TABLE *table, uint64_t *value, int len) {
-    uint64_t index;
+    unsigned int index;
 
     // Empty table
     if (table->nmemb == 0) {
+        printf("Error: the dictionary is empty, nothing to remove.\n");
         return -1;
     }
 
     // Element not found
     if (!htable_search(table, value, len, &index)) {
+        printf("Error: the element to remove is not in the dictionary.\n");
         return -1;
     }
     free(table->entries[index].value);
