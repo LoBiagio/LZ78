3e7d98ff96189474e8ec9ee8b71fcd2e66ef13f1
framola <francimola@hotmail.it>
2015-05-18 19:32:52 +0200

    Decompressor finished with problems

--- decompressor.c
+++ decompressor.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include "htable.h"
 #include "bitio.h"
+#include <errno.h>
+#include <string.h>
+#include <math.h>
 #define DICT_SIZE 2000
 typedef struct
 {
@@ -17,7 +20,7 @@ struct darray
 	DENTITY* dictionary;
  	unsigned int nmemb;
  	unsigned int dim;
-}
+};
 
 struct darray *
 array_new(unsigned int size)
@@ -35,13 +38,20 @@ array_new(unsigned int size)
 	tmp->nmemb = 0;
 	tmp->dim = size;
 	return tmp;		
-}
+};
 
+/*
+ * Returns the number of entries currently in the dictionary
+ */
 int
 insert (struct darray *da, unsigned int father, unsigned char value)
 {
-	da->dictionary[da->nmemb].father = father;
-	da->dictionary[da->nmemb].value = value;
+	if(father > 0){
+		da->dictionary[da->nmemb].father = father;
+		da->dictionary[da->nmemb].value = value;
+		da->nmemb++;
+	}
+	return da->nmemb;
 }
 
 unsigned int
@@ -49,9 +59,62 @@ get_size (struct darray* da)
 {
 	return da->nmemb;
 }
-
+//explore the tree from bottom to top and rfill the buffer with the read characters, returns the number of characters in the buffer
+//in value there is the missing character of the previous call
+unsigned int
+explore_darray (struct darray *da, unsigned int index, char *buf, unsigned char *value)
+{
+	unsigned int offset; //counter from the bottom of the buffer (same size as the dictionary)
+	if ( (da == NULL) || (buf == NULL) ){
+		errno = EINVAL;
+		return -1;
+	}
+	offset = da->dim-1; 
+	if ( index < 256 ){ //we are already at the first level of the tree
+		*value = (unsigned char)index;
+		buf[offset] = index;
+		return 1;
+	}
+	
+	while (da->dictionary[index-256].father >= 256){ //explore the tree
+		buf[offset] = da->dictionary[index-256].value;
+		offset--;
+		index = da->dictionary[index-256].father;
+	}
+	buf[offset] = da->dictionary[index-256].value;
+	buf[--offset] = (unsigned char)da->dictionary[index-256].father;
+	*value = (unsigned char)da->dictionary[index-256].father;
+	return da->dim - offset -1;
+}
 
 int main() {
-
-
+	int fd_w;
+	int ret;
+	uint64_t tmp;
+	unsigned char old_value;
+	struct bitio *fd_r;
+	struct darray *da;
+	unsigned char *buf;
+	unsigned int father = 0, buf_len;
+	fd_r = bitio_open ("compressed", 'r');
+	fd_w = open ("B_NEW", (O_CREAT | O_TRUNC | O_WRONLY) , 0666);
+	if( (da = array_new(DICT_SIZE)) == NULL){
+		perror("error on array_new");
+	}
+	buf = calloc(DICT_SIZE, sizeof(unsigned char));
+	while( (ret = bitio_read (fd_r, &tmp, (int)log2(da->nmemb + 256) ) > 0 )){
+		buf_len = explore_darray(da, (unsigned int)tmp, buf, &old_value);
+		insert(da, father, old_value);
+		printf("%d\n",father);
+		printf("%c\n",old_value);
+		//fflush(0);
+		father = (unsigned int)tmp;
+		printf("%d\n",father);
+		write(fd_w, &buf[da->dim - buf_len], buf_len);
+	}
+	write(fd_w, &old_value, 1);
+	bitio_close(fd_r);
+	close(fd_w);
+	return 0;	 
 }
+
