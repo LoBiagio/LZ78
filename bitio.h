13d5fcad8aa3f2f85f7f81f5635f44b01e4eb114
Lorenzo Biagini <lorebiagio@gmail.com>
2015-05-28 17:31:51 +0200

    bitio_flush removed from header
    bitio_flush is now a static function: to be sure to write down in the file all the data, use bitio_close.

--- bitio.h
+++ bitio.h
@@ -7,7 +7,6 @@ struct bitio;
 struct bitio *bitio_open(const char*, char);
 int bitio_write(struct bitio*, uint64_t *, int);
 int bitio_read(struct bitio*, uint64_t *, int);
-int bitio_flush(struct bitio*);
 int bitio_close(struct bitio*);
 
 #endif // BITIO_H_INCLUDED
